723110db3c0a3df38938d37f48eee840729f25d7
digitaltradeYazilim <yazilim@digitaltrade.com.tr>
2019-05-27 16:52:35 +0200

    final

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Model\Review;
 use App\Model\Product;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Http\Requests\ReviewRequest;
 use App\Http\Resources\ReviewResource;
 use Symfony\Component\HttpFoundation\Response;
@@ -68,16 +69,11 @@ class ReviewController extends Controller
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Review  $review
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Review $review)
+    public function update(Request $request, Review $review ,Product $product)
     {
-        //
+        $review->update($request->all());
+        
+        return response(['data' => new ReviewResource($review)],Response::HTTP_CREATED);
     }
 
     /**
@@ -86,8 +82,9 @@ class ReviewController extends Controller
      * @param  \App\Model\Review  $review
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Review $review)
+    public function destroy(Product $product, Review $review)
     {
-        //
+        $review->delete();
+        return response(null,Response::HTTP_NO_CONTENT);
     }
 }
